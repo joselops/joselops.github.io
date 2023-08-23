cb6475bc3cef49a80047d48618bf9fb3052dc56a
lops <joseloepezsantacruz@gmail.com>
2023-08-23 20:03:53 +0200

    scrollDuration

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
     "use strict";
     
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
+        scrollDuration : 100, // smoothscroll duration
         mailChimpURL   : ''   // mailchimp url
     },
 
